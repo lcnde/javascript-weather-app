9146726761ff40a4d63e0f00b0fe9a373c233656
lcnde <176673026+lcnde@users.noreply.github.com>
2021-09-02 11:40:51 +0200

    update (not working)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import _ from 'lodash';
+
+var key = ""
+
+(async () => {
+  return key = fetch(`${process.env.OPENWEATHER_API}`)
+})();
+
+const keyelement = document.createElement("p")
+keyelement.innerHTML = `${key}`
+
+const test = document.querySelector('.test');
+
+test.appendChild(keyelement);
