569239a3c7cd3c92117e677ed80193ed77357157
lcnde <176673026+lcnde@users.noreply.github.com>
2021-09-03 08:06:02 +0200

    feature: working form that outputs to console data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import _ from 'lodash';
 
-var key = ""
+var apiKey = process.env.OPENWEATHER_API;
 
-(async () => {
-  return key = fetch(`${process.env.OPENWEATHER_API}`)
-})();
+const weather = async function weather(cityName) {
+  let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`, {mode: 'cors'});
+  let data = await response.json();
+  return data;
+};
 
-const keyelement = document.createElement("p")
-keyelement.innerHTML = `${key}`
+async function formSubmit() {
+  var cityWeather = await weather(document.frm1.city.value);
+  console.log(cityWeather);
+};
 
-const test = document.querySelector('.test');
+const search = document.getElementById('formSubmit');
+search.addEventListener("click", ()=> {
+  formSubmit();
+});
 
-test.appendChild(keyelement);
+
