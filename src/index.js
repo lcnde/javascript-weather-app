5ea61be746b58c6b96655c78793fc3d012efe065
lcnde <176673026+lcnde@users.noreply.github.com>
2021-09-03 19:39:07 +0200

    switched to development

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 // using only process.env.OPENWEATHER_API was bundling the real value of the key inside main.js exposing the api key to the public. By putting the key inside a function its value is protected since the value literal does not get exposed inside main.js
 function getAPIKey() {
   return process.env.OPENWEATHER_API
-}
+};
 
 const weather = async function weather(cityName) {
   let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${getAPIKey()}`, {mode: 'cors'});
@@ -13,12 +13,11 @@ const weather = async function weather(cityName) {
 
 async function formSubmit() {
   var cityWeather = await weather(document.frm1.city.value);
-  console.log(cityWeather);
+  console.log(cityWeather.name);
 };
 
 const search = document.getElementById('formSubmit');
 search.addEventListener("click", ()=> {
   formSubmit();
 });
-
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.js',
   plugins: [
     new Dotenv()
