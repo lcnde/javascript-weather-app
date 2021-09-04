718527a06748dc01257e8318091c13f201a02bb0
lcnde <176673026+lcnde@users.noreply.github.com>
2021-09-04 20:03:41 +0200

    features: loader, stuff container
    the loader works, when the request completes it disappears. The container appears if it is not already present. Between requests the container is emptied of all its nodes so it can accomdoate new nodes with new info from the new request.

--- src/index.js
+++ src/index.js
@@ -8,15 +8,69 @@ const weather = async function weather(cityName) {
   return data;
 };
 
-
-async function formSubmit() {
-  // put here the function that loads the gif with the loading screen
-  var cityWeather = await weather(document.frm1.city.value);
-  console.log(cityWeather);
-};
-
+// adds the event listener on the form button
 const search = document.getElementById('formSubmit');
 search.addEventListener("click", ()=> {
   formSubmit();
 });
+
+// this function will take the data from the json and will display it in the page
+async function formSubmit() {
+  forecastContainer();
+  loader.add();
+  var cityWeather = await weather(document.frm1.city.value);
+  loader.remove();
+  console.log(cityWeather);
+};
+
+// this creates the forecast container if it does not exist already
+function forecastContainer() {
+  var element = document.querySelector('.forecast-container');
+  if (typeof(element) == 'undefined' || element == null) { //creates the container if it does not exist
+    var element = document.createElement('div');
+    element.classList.add('forecast-container');
+    document.querySelector('.main').appendChild(element);
+  } else { //empties the forecast container so it can get filled with new info between each request
+    if (typeof(element) != 'undefined' || element != null) {
+      var child = element.lastElementChild;
+      if (typeof(child) != 'undefined' || child != null) {
+        while (child) {
+          element.removeChild(child);
+          child = element.lastElementChild;
+        };
+      };
+    };
+  };
+};
+
+// function previsionContainer() {
+
+// }
+
+// creates a loader animation anad removes it once the api request finishes
+const loader = {
+  add: () => {
+    var loader = document.createElement("div");
+    loader.classList.add("loader");
+    document.querySelector('.forecast-container').appendChild(loader);
+    var div = document.createElement("div");
+    div.classList.add("lds-ripple");
+    document.querySelector(".loader").appendChild(div);
+    var div = document.createElement("div");
+    document.querySelector(".lds-ripple").appendChild(div);
+    document.querySelector(".lds-ripple").appendChild(div);
+  },
+  remove: () => {
+    var e = document.querySelector(".loader");
+    // loop that removes all child noodes
+    var child = e.lastElementChild;
+    while (child) {
+      e.removeChild(child);
+      child = e.lastElementChild;
+    }
+    e.remove(); //removes node
+  }
+};
+
+
 
